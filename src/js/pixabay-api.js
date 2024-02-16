9640db3236d60fc209db6d289af3bedd38b331db
JackRybnikov <jackrybnikov008@gmail.com>
2024-02-16 21:04:28 +0200

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,17 @@
 import { galleryCreation } from "./render-functions.js";
 
 export const sendRequest = (search) => {
-    const imageToAdd = {
+
+    let addToSearch = new URLSearchParams({
         key: "42388767-1d9ccb99c091176bae08c2c78",
-        q: `${search}`,
+        q: search,
         image_type: 'photo',
         orientation: 'horizontal',
         safesearch: true,
         per_page: 198,
-    };
+    });
 
-    let addToSearch = '?';
-    for (let key in imageToAdd){
-        addToSearch +=`${key}=${imageToAdd[key]}&`;
-    }
-
-    fetch(`https://pixabay.com/api/${addToSearch}`)
+    fetch(`https://pixabay.com/api/?${addToSearch}`)
     .then(response => {
         if (!response.ok) {
           throw new Error(response.status);
